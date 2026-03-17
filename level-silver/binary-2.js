d90f85459c7e5b0b027822050e846767643e4ac3
seongmin36 <whtjdals3625@gmail.com>
2026-03-18 01:54:50 +0900

    solve: 2805

--- level-silver/binary-2.js
+++ level-silver/binary-2.js
@@ -1,21 +1,33 @@
 // 나무 자르기 [2805]
+
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
 
 const line1 = input[0].split(" ").map(Number);
-const line2 = input[1].split(" ").map(Number);
+const tree_need = line1[1];
+const trees = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
-line2.sort((a, b) => b - a);
-for (let tree_h = line2[0]; tree_h > line2[line1[0] - 1]; tree_h--) {
-  let tree_m = 0;
-  let tree_maintain = 0;
-  for (let i = 0; i < line1[0]; i++) {
-    tree_maintain = line2[i] - tree_h;
+let low = 0;
+let high = trees[trees.length - 1];
+let result = 0;
 
-    tree_m += tree_maintain;
-    if (line1[1] === tree_m) {
-      console.log(tree_h);
-      return 0;
+while (low <= high) {
+  let sum = 0;
+  let pivot = Math.floor((low + high) / 2);
+  trees.forEach((tree) => {
+    if (tree > pivot) {
+      sum += tree - pivot;
     }
+  });
+  if (sum >= tree_need) {
+    result = pivot;
+    low = pivot + 1;
+  } else {
+    high = pivot - 1;
   }
 }
+
+console.log(result);
