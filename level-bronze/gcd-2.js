b21d482d98fe128097b6a703c82c7f21c6e34a0a
seongmin36 <whtjdals3625@gmail.com>
2026-03-17 22:36:38 +0900

    solve: 9613

---
+++ level-bronze/gcd-2.js
@@ -0,0 +1,19 @@
+// GCD 합 [9613]
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
+const line = Number(input[0]);
+
+for (let i = 1; i <= line; i++) {
+  const arr = input[i].split(" ").map(Number);
+  const test_case = arr[0];
+  const new_arr = arr.slice(1);
+
+  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+  let sum_gcd = 0;
+  for (let g = 0; g < line; g++) {
+    for (let h = g + 1; h < test_case; h++)
+      sum_gcd += gcd(new_arr[g], new_arr[h]);
+  }
+
+  console.log(sum_gcd);
+}
