d0666a49a102778c82a7a3c5c7b9aea1652c2a6c
seongmin36 <whtjdals3625@gmail.com>
2026-04-06 15:45:26 +0900

    docs: 주석 추가

--- leetcode/168-ExcelSheetColumnTitle.js
+++ leetcode/168-ExcelSheetColumnTitle.js
@@ -5,6 +5,7 @@
 // 논리로 풀기 or ascii code로 풀기
 // A의 ascii code는 65번 -> String.fromCharCode() : 인자 number값을 ascii코드로 반환
 // result=result+A와 result=A+result의 결과값은 다르다. (위치가 다름)
+// 함정은 A가 0부터 시작하지 않는다는 점이다. 따라서 일반적인 진법변환 방식에서 -1을 빼야한다.
 function convertToTitle(columnNumber) {
   let result = "";
 
